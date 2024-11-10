fed44645997ca7d11efd085f22147b2dc86d04c6
Parksaeroyi1 <142122264+Parksaeroyi1@users.noreply.github.com>
2024-11-10 18:29:07 -0500

    Added feeback with new project

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// Array of image paths
+// Array of image paths //
 const images = ['images/russell.jpg', 'images/homers.png', 'images/rockpap.jpg'];
 
-// Selects a random image from the array
+// Selects a random image from the array //
 const randomImage = images[Math.floor(Math.random() * images.length)];
 
-// Queries the image element and sets its source to the random image
+// Queries the image element and sets its source to the random image //
 const randomProjectImg = document.querySelector(".random_image");
 randomProjectImg.src = randomImage;
 
-// JSON string containing profile information
+// JSON string containing profile information //
 const jsonAbout = ` { 
     "title": "Elijah Suyat",
     "about": "I’m currently on my journey to becoming a software developer. Passionate about coding and eager to learn, I am open to new job opportunities where I can grow and contribute to exciting projects.",
     "cover_image": "images/me.jpg"
 }`;
 
-// Parses the JSON string into an object
+// Parses the JSON string into an object //
 const parsedAbout = JSON.parse(jsonAbout);
 
-// Function to display the profile information on the webpage
+// Function to display the profile information on the webpage //
 const displayAbout = () => {
     const aboutSection = document.querySelector('.about_me_content');
 
@@ -49,12 +49,12 @@ const myProfile = () => {
     }
 };
 
-// Queries the hamburger menu element and the navigation links container
+// Queries the hamburger menu element and the navigation links container //
 
 const hamburger = document.getElementById('hamburger');
 const navLinks = document.querySelector('.header_nav');
 
-// Adds a click event listener to the hamburger menu to toggle the 'show' class on the navigation links
+// Adds a click event listener to the hamburger menu to toggle the 'show' class on the navigation links //
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('show');
@@ -75,7 +75,7 @@ projectLinks.forEach(link => {
 
 // Feedback //
 
-// Toggle visibility of feedback modal
+// Toggle visibility of feedback modal //
 const toggle_visibility = () => {
     const feedback = document.getElementById('feedback-main');
     const overlay = document.getElementById('overlay');
